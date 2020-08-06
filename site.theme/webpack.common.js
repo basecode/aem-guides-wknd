12a9547b26b44f85e94701417569c911ae15285e
Vlad Băilescu <bailescu@adobe.com>
2020-08-06 16:13:17 +0300

    Consolidated SK name to a single variable

--- site.theme/webpack.common.js
+++ site.theme/webpack.common.js
@@ -9,6 +9,7 @@ const CopyWebpackPlugin       = require('copy-webpack-plugin');
 const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 
 const SOURCE_ROOT = __dirname + '/src/main/webpack';
+const SITE = 'wkndsk';
 
 module.exports = {
         resolve: {
@@ -21,7 +22,7 @@ module.exports = {
             site: SOURCE_ROOT + '/site/main.js'
         },
         output: {
-            filename: 'jcr_root/content/wkndsk/_theme/js/[name].bundle.js',
+            filename: 'jcr_root/content/' + SITE + '/_theme/js/[name].bundle.js',
             path: path.resolve(__dirname, 'dist')
         },
         optimization: {
@@ -97,14 +98,14 @@ module.exports = {
             new CleanWebpackPlugin(),
             new webpack.NoEmitOnErrorsPlugin(),
             new MiniCssExtractPlugin({
-                filename: 'jcr_root/content/wkndsk/_theme/css/[name].bundle.css',
+                filename: 'jcr_root/content/' + SITE + '/_theme/css/[name].bundle.css',
             }),
             new TSLintPlugin({
                 files: [SOURCE_ROOT + '/**/*.ts', SOURCE_ROOT + '/**/*.tsx'],
                 config: './tslint.json'
             }),
             new CopyWebpackPlugin([
-                { from: path.resolve(__dirname, SOURCE_ROOT + '/resources'), to: './jcr_root/content/wkndsk/_theme/resources' }
+                { from: path.resolve(__dirname, SOURCE_ROOT + '/resources'), to: './jcr_root/content/' + SITE + '/_theme/resources' }
             ])
         ],
         stats: {
