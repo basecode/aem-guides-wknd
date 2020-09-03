e7f327e40fd3890ca2f5e9463df635b7c915f668
Bartosz Glowacki <bartosz.glow@gmail.com>
2020-09-03 15:13:54 +0200

    starterkit builds cleanup

--- site.theme/webpack.dev.js
+++ site.theme/webpack.dev.js
@@ -9,11 +9,6 @@ module.exports = merge(common, {
    mode: 'development',
    devtool: 'inline-source-map',
    performance: { hints: "warning" },
-   plugins: [
-      new HtmlWebpackPlugin({
-         template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
-      })
-   ],
    devServer: {
       inline: true,
       proxy: [{
